aae35eeb931f67c0cfb7dc528f53729d57b7ca9f
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-10 00:47:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ st.title("OFFICE OF THE SSE/PW/SGAM")
 letter_type = st.selectbox("Select Letter Type", list(template_files.keys()))
 if letter_type != "Update Employee Database":
     # Employee dropdown and Letter Date
-    df = employee_master["Sheet1"]  # Replace with correct sheet name
+    df = employee_master["Apr.25"]  # Replace with correct sheet name
     df["Display"] = df.apply(lambda r: f"{r['PF No.']} - {r['SF-11 short name']} - {r['UNIT / MUSTER NUMBER']} - {r['Employee Name']}", axis=1)
     selected = st.selectbox("Select Employee", df["Display"].dropna())
     row = df[df["Display"] == selected].iloc[0]
