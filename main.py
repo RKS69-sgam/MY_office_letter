9632349965e91250289756e3a31db5fec316ee51
RKS69-sgam <ramakantshukla69@gmail.com>
2025-07-10 23:51:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -503,7 +503,7 @@ if st.button("Generate Letter"):
         class_df.to_excel(class_file, sheet_name="Sheet1", index=False)
         st.success("Register updated.")
 
-if letter_type in ["Engine Pass Letter", "Card Pass Letter"]:
+  if letter_type in ["Engine Pass Letter", "Card Pass Letter"]:
     if letter_type == "Engine Pass Letter":
         template_path = "assets/Engine Pass letter temp.docx"
         save_name = f"EnginePass-{context['EmployeeName'].strip()}.docx"
@@ -517,4 +517,5 @@ if letter_type in ["Engine Pass Letter", "Card Pass Letter"]:
     word_path = generate_word(template_path, context, save_name)
     st.success("Letter generated successfully.")
     download_word(word_path)
-       
+
+    
